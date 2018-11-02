4a639c699c3a20a64a529ee6de8dd1c60ce7bbe7
StevenMHernandez <contact@stevenhdesigns.com>
2018-11-01 20:18:38 -0400

    initial commit: create resizing script, create basic html screen, create simple api endpoint to collect available images

---
+++ server.js
@@ -0,0 +1,32 @@
+const { execFile } = require('child_process');
+const gifsicle = require('gifsicle');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 3000;
+const resizeWidth = 64;
+
+var images = [];
+
+fs.readdir("images/original", function(err, items) {
+	images = items;
+
+	items.forEach(function(filename) {
+		execFile(gifsicle, ['--resize-fit-width', resizeWidth, '-o', 'images/resized/' + filename, 'images/original/' + filename], err => {
+		  if (err) {
+		    throw err;
+		  }
+
+		  console.log(filename + ' resized');
+		});
+	});
+});
+
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/images', (req, res) => res.json(images));
+app.use(express.static('images'));
+
+app.listen(port, () => console.log(`Server started on port ${port}!`));
+
+
