bc28638a4cf68cfb56d5bf9bde69e9f09844092c
StevenMHernandez <contact@stevenhdesigns.com>
2018-11-19 21:00:47 -0500

    add state diagram to make the image changes more logical, add menus options to interact with pet

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const resizeWidth = 64;
 var images = [];
 
 fs.readdir("images/original", function(err, items) {
-	images = items.filter((filename) => filename != ".gitignore");
-console.log(images);
+	images = items.filter((filename) => filename[0] != ".");
+	console.log(images);
 
 	images.forEach(function(filename) {
 		execFile(gifsicle, ['--resize-fit-width', resizeWidth, '-o', 'images/resized/' + filename, 'images/original/' + filename], err => {
@@ -27,6 +27,7 @@ console.log(images);
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 app.get('/images', (req, res) => res.json(images));
 app.use(express.static('images'));
+app.use(express.static('node_modules'));
 
 app.listen(port, () => console.log(`Server started on port ${port}!`));
 
